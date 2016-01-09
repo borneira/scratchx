8879b7daa182150e5aaa53e1603271b4905039cc
borneira <juan+git@borneira.com>
2016-01-09 20:01:56 +0100

    1

--- test.js
+++ test.js
@@ -1,4 +1,54 @@
 (function(ext) {
+
+    sauron_ve_movimiento = false;
+    dataversion = 0;
+    loadtime = 0;
+
+    switches = [];
+    dimmers = [];
+    wcovers = [];
+    sensors = [];
+    temp_sensors = [];
+    light_sensors = [];
+
+    mswitches = [];
+
+    $.ajax({
+        url: 'http://192.168.1.112/port_3480/data_request?id=lu_sdata',
+        async: false,
+        success: function( data ) {
+            // Got the data - parse it and return the temperature
+            //dataParsed = $.parseJSON(data);
+            dataversion = data.dataversion;
+            loadtime = data.loadtime;
+            for (i=0 ; i< data.devices.length ; i++) {
+                switch (data.devices[i].category) {
+                    case 3:
+                        switches.push(data.devices[i]);
+                        break;
+                    case 4:
+                        sensors.push(data.devices[i]);
+                        break;
+                    case 8:
+                        wcovers.push(data.devices[i]);
+                        break;
+                    case 17:
+                        temp_sensors(data.devices[i]);
+                        break;
+                    case 18:
+                        light_sensors(data.devices[i]);
+                        break;
+                }
+            }
+        }
+    });
+
+    for (i=0;i < switches.length ; i++) {
+        mswitches.push(switches[i].name);
+    }
+    var menu = new Object();
+    menu.mswitches = mswitches;
+
     // Cleanup function when the extension is unloaded
     ext._shutdown = function() {};
 
@@ -41,24 +91,50 @@ ext.set_alarm = function(time) {
        }, time*1000);
     };
 
-    ext.when_alarm = function() {
+    ext.when_movimiento = function() {
        // Reset alarm_went_off if it is true, and return true
        // otherwise, return false.
-       if (alarm_went_off === true) {
-           alarm_went_off = false;
+       if (sauron_ve_movimiento === true) {
+           sauron_ve_movimiento = false;
            return true;
        }
 
        return false;
+    };
+    ext.encender = function(devicename) {
+        url = '';
+        for (i=0;i<switches.length;i++) {
+            if (switches.name==devicename) {
+                url = 'http://192.168.1.112/port_3480/data_request?id=lu_action&DeviceNum=' + switches.id;
+                url = url + '&serviceId=urn:upnp-org:serviceId:SwitchPower1&action=SetTargetValue=1';
+                break;
+            }
+        }
+        if (url=='') return 0;
+        $.ajax({url: url, async: false });
+    };
+
+    ext.apagar = function(devicename) {
+        url = '';
+        for (i=0;i<switches.length;i++) {
+            if (switches.name==devicename) {
+                url = 'http://192.168.1.112/port_3480/data_request?id=lu_action&DeviceNum=' + switches.id;
+                url = url + '&serviceId=urn:upnp-org:serviceId:SwitchPower1&action=SetTargetValue=0';
+                break;
+            }
+        }
+        if (url=='') return 0;
+        $.ajax({url: url, async: false });
     };
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
             [' ', 'Encender Luz Pecera', 'set_pecera_on'],
             [' ', 'Apagar Luz Pecera', 'set_pecera_off'],
+            [' ', 'Encender %m.mswitches', 'encender'],
+            [' ', 'Apagar %m.mswitches', 'apagar'],
             ['R', 'Luz SAURON', 'get_luz'],
-            ['', 'run alarm after %n seconds', 'set_alarm', '2'],
-            ['h', 'when alarm goes off', 'when_alarm'],
+            ['h', 'Cuando SAURON detecte movimiento', 'when_movimiento']
         ]
     };
 
